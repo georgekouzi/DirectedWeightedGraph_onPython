cc14f2d18f4fefc367c06a0cd7a3d143082ca166
georgekouzi <57435555+georgekouzi@users.noreply.github.com>
2021-01-11 12:26:02 +0200

    Add files via upload

--- src/Ex3_main.py
+++ src/Ex3_main.py
@@ -1,4 +1,3 @@
-
 from src.DiGraph import DiGraph
 from src.GraphAlgo import GraphAlgo
 
@@ -12,11 +11,11 @@ def check():
     (3.4, [0, 1, 2, 3])
     [[0, 1], [2], [3]]
     (2.8, [0, 1, 3])
-    (inf, None)
+    (inf, [])
     2.062180280059253 [1, 10, 7]
     17.693921758901507 [47, 46, 44, 43, 42, 41, 40, 39, 15, 16, 17, 18, 19]
     11.51061380461898 [20, 21, 32, 31, 30, 29, 14, 13, 3, 2]
-    inf None
+    inf []
     [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47]]
     """
@@ -47,7 +46,6 @@ def check0():
     g_algo = GraphAlgo(g)
     print(g_algo.shortest_path(0, 3))
     g_algo.plot_graph()
-
 
 
 def check1():
@@ -85,6 +83,7 @@ def check2():
     print(g_algo.connected_component(0))
     print(g_algo.connected_components())
     g_algo.plot_graph()
+
 
 if __name__ == '__main__':
     check()
